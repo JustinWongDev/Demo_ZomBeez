1dee57dca45d11563452a4d9b04240a08c3a8ec7
Justin Wong <justincywong12@gmail.com>
2021-01-07 12:33:09 +1000

    WaypointGraph + Environment
    Environment drafted; nodes placed; nodes linked to neighbours.

---
+++ Assets/Features/Nodes/LinkedNode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LinkedNode : MonoBehaviour
+{
+    public int index;
+    public GameObject[] linkedNodeObjects;
+    public int[] linkedNodeIndexes;
+
+    private void Start()
+    {
+        //Assign correct index for each linked Node
+        linkedNodeIndexes = new int[linkedNodeObjects.Length];
+
+        for(int i = 0; i < linkedNodeIndexes.Length; i++)
+        {
+            linkedNodeIndexes[i] = linkedNodeObjects[i].GetComponent<LinkedNode>().index;
+        }
+    }
+
+    private void Update()
+    {
+        DrawLineToLinkedNodes();
+    }
+
+    void DrawLineToLinkedNodes()
+    {
+        foreach (GameObject linkedNode in linkedNodeObjects)
+        {
+            Debug.DrawLine(transform.position, linkedNode.transform.position, Color.green);
+        }
+    }
+}
---
+++ Assets/Features/Nodes/WaypointGraph.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointGraph : MonoBehaviour
+{
+    public GameObject[] graphNodes;
+
+    private void Start()
+    {
+        IndexNodes();
+    }
+
+    void IndexNodes()
+    { 
+        for(int i = 0; i < graphNodes.Length; i++)
+        {
+            graphNodes[i].GetComponent<LinkedNode>().index = i;    
+        }
+    }
+}
