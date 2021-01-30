ec342f987656cac0e490ced2e262acfd31ea6a6e
Justin Wong <justincywong12@gmail.com>
2021-01-30 14:28:13 +1000

    Cinemachine
    Tested and implemented basic func for transitioning between menu and gameplay;

---
+++ Assets/Features/Camera/CinemachineController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CinemachineController : MonoBehaviour
+{
+    [SerializeField] private CinemachineBrain camBrain;
+    [SerializeField] private CinemachineVirtualCamera[] cams; //0 menu; 1 default game
+
+    public void SwitchToCam(int index)
+    {
+        for (int i = 0; i < cams.Length; i++)
+        {
+            if (i == index)
+            {
+                //cams[i].enabled = true;
+                cams[i].Priority = 10;
+            }
+            else
+            {
+                //cams[i].enabled = false;
+                cams[i].Priority = 0;
+            }
+        }
+    }
+}
