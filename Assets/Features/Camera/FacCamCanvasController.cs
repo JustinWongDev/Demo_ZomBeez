17461984e88d742f1b0f3040ba7597ad622682a7
Justin Wong <justincywong12@gmail.com>
2021-02-18 10:18:35 +1000

    Art: Facility Cam Overlay
    Camera corners, date and time, cam name;

---
+++ Assets/Features/Camera/FacCamCanvasController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+using TMPro;
+using static System.DateTime;
+
+public class FacCamCanvasController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI t_camName = null;
+    [SerializeField] private TextMeshProUGUI t_date = null;
+    [SerializeField] private TextMeshProUGUI t_time = null;
+    [SerializeField] private CinemachineClearShot facCamController = null;
+
+    private void Update()
+    {
+        SetCamName();
+        TickDateTime();
+    }
+
+    private void TickDateTime()
+    {
+        t_time.text = DateTime.Now.TimeOfDay.Hours.ToString() + ":" + DateTime.Now.TimeOfDay.Minutes.ToString() + ":" + DateTime.Now.TimeOfDay.Seconds.ToString("00") + ":" + DateTime.Now.TimeOfDay.Milliseconds.ToString("000");
+        t_date.text = DateTime.Today.Day.ToString() + " / " + DateTime.Today.Month.ToString() + " / " + DateTime.Today.Year.ToString();
+    }
+
+    private void SetCamName()
+    {
+        t_camName.text = facCamController.LiveChild.Name.ToString();
+    }
+}
---
+++ Assets/Features/General/EnableInGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableInGame : MonoBehaviour
+{
+    void Start()
+    {
+        GameManager.live.OnGameStart += EnableOnGameStart;
+        this.gameObject.SetActive(false);
+    }
+
+    void EnableOnGameStart()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
